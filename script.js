326366ad62f6dfad8a3a1a9933a67097fdbfda81
Ankit Dalsaniya <99668053+Ankitcreedally@users.noreply.github.com>
2022-02-15 10:25:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.sliksliderr').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+    $('.slick-prev').text('');
+    $('.slick-next').text('');
+  });
