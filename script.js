08b6a2daa0b21433aaaf6ded7c551e89776206f4
Ankit Dalsaniya <99668053+Ankitcreedally@users.noreply.github.com>
2022-02-16 13:35:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -20,16 +20,43 @@ $(document).ready(function () {
     $('.slick-prev').text('');
     $('.slick-next').text('');
 
-    jQuery('.toggle-nav').click(function(e) {
-		jQuery(this).toggleClass('active');
-		jQuery('.menu ul').toggleClass('active');
+    jQuery('.toggle-nav').click(function (e) {
+        jQuery(this).toggleClass('active');
+        jQuery('.menu ul').toggleClass('active');
 
-		e.preventDefault();
-	});
+        e.preventDefault();
+    });
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
+    $(window).scroll(function () {
+        var bn = $('#floating_search');
+        if ($(window).scrollTop() > 300) {
+            bn.css({ position: 'fixed', bottom: '20px' , display: 'block'});
+        }
+        else {
+            bn.css('position', 'static');
+        }
+    });
+    var bn = $('#floating_search');
+    bn.css( 'display', 'none' );
+
+    jQuery('#floating_search').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0});
+        return false;
+    })
 
 });
 
 function toggelnav(x) {
     x.classList.toggle("change");
     $('.mobilenav').toggle("show");
-  }
+}
+
